cf85703cabc9c4c695a8e8bdf8bf570374724a19
Romain Berland <romainberland03@gmail.com>
2020-03-30 16:34:06 +0200

    fix static3

--- mon_projet/mon_projet/urls.py
+++ mon_projet/mon_projet/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
