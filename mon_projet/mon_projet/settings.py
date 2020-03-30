ba461ed0ac6ff4350e5943e61dfcdffb1f497474
Romain Berland <romainberland03@gmail.com>
2020-03-30 15:02:18 +0200

    fix bdd

--- mon_projet/mon_projet/settings.py
+++ mon_projet/mon_projet/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'mon_projet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': 'postgresql-<romainb>.alwaysdata.net',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', ,
+        'NAME': 'romainb_db',
+        'USER': 'romainb',
+        'password': 'Laurag311'
     }
 }
 
